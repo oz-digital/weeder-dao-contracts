192293ca673e01ba6c6f1b59374e75e7645de5be
Alexander Kunekov <fairyfromalfeya@gmail.com>
2023-03-25 02:08:32 +0100

    feat: template

---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.18',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: 'polygon_mumbai',
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+};
+
+export default config;
