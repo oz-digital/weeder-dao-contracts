ff14b3b383dbaa7307d82cc28d48d2023121a005
Alexander Kunekov <fairyfromalfeya@gmail.com>
2023-04-05 15:20:06 +0200

    feat: prod weeder token contract

--- contracts/WeederToken.sol
+++ contracts/WeederToken.sol
@@ -1,39 +1,110 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.18;
 
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 
-import "./interfaces/ITaskManager.sol";
-import "./interfaces/IWeederToken.sol";
+contract WeederToken is
+    Initializable,
+    ERC20Upgradeable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    ERC20PermitUpgradeable,
+    ERC20VotesUpgradeable,
+    UUPSUpgradeable
+{
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
-contract WeederToken is ERC20Upgradeable, IWeederToken {
-    address public taskManager;
-
-    function initialize(
-        address _initialTaskManager,
-        uint256 _initialTotalSupply
-    ) initializer public {
-        __ERC20_init("Weeder DAO Token", "WDR");
-        taskManager = _initialTaskManager;
-        _mint(msg.sender, _initialTotalSupply);
+    constructor() {
+        _disableInitializers();
     }
 
-    function setTaskManager(address _newTaskManager) external override initializer {
-        address previous = taskManager;
+    function decimals()
+        public
+        override
+        pure
+        returns (uint8)
+    {
+        return 8;
+    }
 
-        taskManager = _newTaskManager;
+    function initialize() public initializer {
+        // Roles
+        __AccessControl_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
 
-        emit TaskManagerChanged({ current: _newTaskManager, previous: previous });
+        // ERC20
+        __ERC20_init("WeederToken", "WDT");
+        __ERC20Permit_init("WeederToken");
+        __Pausable_init();
+        __ERC20Votes_init();
+
+        // Mint 20kk tokens to owner
+        _mint(msg.sender, 20_000_000 * 10 ** decimals());
+
+        // UUPS
+        __UUPSUpgradeable_init();
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
     }
 
     function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override {
-        if (from != address(0)) {
-            ITaskManager(taskManager).accrueMarketUsers(from, to);
-        }
+        address _from,
+        address _to,
+        uint256 _amount
+    )
+        internal
+        override
+        whenNotPaused
+    {
+        super._beforeTokenTransfer(_from, _to, _amount);
     }
+
+    function _afterTokenTransfer(
+        address _from,
+        address _to,
+        uint256 _amount
+    )
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._afterTokenTransfer(_from, _to, _amount);
+    }
+
+    function _mint(address _to, uint256 _amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._mint(_to, _amount);
+    }
+
+    function _burn(address _account, uint256 _amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._burn(_account, _amount);
+    }
+
+    function _authorizeUpgrade(address _newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
 }
