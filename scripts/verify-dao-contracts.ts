456c0594af4a42f1a73098dac8640ebbb83be2d0
Alexander Kunekov <fairyfromalfeya@gmail.com>
2023-04-08 20:36:37 +0200

    feat: updated market address for verify script

--- scripts/verify-dao-contracts.ts
+++ scripts/verify-dao-contracts.ts
@@ -7,7 +7,7 @@ const verify = async (contractAddress: string, args: any[]): Promise<void> =>
   });
 
 const main = async (): Promise<void> => {
-  await verify('0x2F4062C7746EF3f1A806dE29C3375C1D2714b303', []);
+  await verify('0xa42C2152bDA5fD130fB2B0098dC21Ee35d20D2f9', []);
   await verify('0xE11702CF04621D395E75680C29d3dbF81484e875', []);
 };
 
