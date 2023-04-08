04b1a24b8804291737db6536e36c9d1040dd858d
Alexander Kunekov <fairyfromalfeya@gmail.com>
2023-04-08 16:57:08 +0200

    fix: moved dividends collected event to state interface

--- contracts/interfaces/market/IMarketDividend.sol
+++ contracts/interfaces/market/IMarketDividend.sol
@@ -2,13 +2,6 @@
 pragma solidity 0.8.18;
 
 interface IMarketDividend {
-    event DividendsCollected(
-        uint256 amount,
-        address indexed token,
-        address indexed recipient,
-        address indexed sender
-    );
-
     function availableTokenDividends(address _account, address _token) external view returns (uint256);
 
     function accrueAccountDividends(address _account) external;
